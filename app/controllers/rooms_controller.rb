b2b1b49787a1b4d3ee5f639aa330c2a87a4dcc25
akifuji-tonakai <akifujitonakai@icloud.com>
2021-02-01 09:38:15 +0900

    画像表示の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -23,7 +23,8 @@ class RoomsController < ApplicationController
   end
 
   private
+
   def room_params
-    params.require(:room).permit(:name, user_ids:[])
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
